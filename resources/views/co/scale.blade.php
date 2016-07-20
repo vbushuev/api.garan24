2e87d74281a683a1c9a8728fee554cc5c64557bd
vbushuev <v.bushuev.co@gmail.com>
2016-07-21 00:31:46 +0300

    Scale added triangle to end

--- resources/views/co/scale.blade.php
+++ resources/views/co/scale.blade.php
@@ -1,15 +1,9 @@
-<ul class="scale">
-    <li class="scale-item helped @if(isset($section)&&in_array($section,["contact","delivery","passport","payment"]))active @endif" data-helper="helper-contact">
-        <!--@if(isset($section)&&$section=="contact")
-            <i class="fa fa-circle" aria-hidden="true"></i>
-
-        @else
-            <i class="fa fa-circle-o" aria-hidden="true"></i>
-        @endif-->
+<div class="scale">
+    <div class="scale-item helped @if(isset($section)&&in_array($section,["contact","delivery","passport","payment"]))active @endif" data-helper="helper-contact">
         Контакты
         <i class="fa fa-angle-double-right" aria-hidden="true"></i>
-    </li>
-    <li class="scale-item helped @if(isset($section)&&in_array($section,["delivery","passport","payment"]))active @endif" data-helper="helper-shipping">Доставка<i class="fa fa-angle-double-right" aria-hidden="true"></i></li>
-    <li class="scale-item helped @if(isset($section)&&in_array($section,["passport","payment"]))active @endif" data-helper="helper-passport">Паспорт<i class="fa fa-angle-double-right" aria-hidden="true"></i></li>
-    <li class="scale-item helped @if(isset($section)&&$section=="payment")active @endif" data-helper="helper-payment">Оплата</li>
-</ul>
+    </div><!--
+    --><div class="scale-item helped @if(isset($section)&&in_array($section,["delivery","passport","payment"]))active @endif" data-helper="helper-shipping">Доставка<i class="fa fa-angle-double-right" aria-hidden="true"></i></div><!--
+    --><div class="scale-item helped @if(isset($section)&&in_array($section,["passport","payment"]))active @endif" data-helper="helper-passport">Паспорт<i class="fa fa-angle-double-right" aria-hidden="true"></i></div><!--
+    --><div class="scale-item helped @if(isset($section)&&$section=="payment")active @endif" data-helper="helper-payment">Оплата</div>
+</div>
